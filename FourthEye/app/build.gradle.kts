e7aa11984783a5036fce729b01e8babab4ef9893
Sayan Sil <sayan227sil@ymail.com>
2025-07-18 22:09:53 +0530

    Add preview

--- FourthEye/app/build.gradle.kts
+++ FourthEye/app/build.gradle.kts
@@ -28,19 +28,25 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
+    kotlin {
+        jvmToolchain(21)
+    }
+    buildToolsVersion = "36.0.0"
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
-
+    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +55,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.view)
+    implementation (libs.androidx.camera.lifecycle)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- FourthEye/app/src/main/java/metro/overcrowded/fourtheye/MainActivity.kt
+++ FourthEye/app/src/main/java/metro/overcrowded/fourtheye/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import metro.overcrowded.fourtheye.composables.CameraPreviewScreen
 import metro.overcrowded.fourtheye.ui.theme.FourthEyeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FourthEyeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FourthEyeTheme {
-        Greeting("Android")
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+    if (cameraPermissionState.status.isGranted) {
+        CameraPreviewScreen()
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize().wrapContentSize().widthIn(max = 480.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val textToShow = "This app needs camera permissions to work ✨\n" +
+                    "We cannot take pictures without the camera :)"
+            Text(textToShow, textAlign = TextAlign.Center)
+            Spacer(Modifier.height(16.dp))
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Allow Camera")
+            }
+        }
     }
 }
